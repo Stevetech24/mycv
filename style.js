8f7ab817027906ee61fa2bc9cd535614d204c1c9
Stevetech24 <madubuikeebere203@gmail.com>
2025-05-28 22:55:24 +0100

    Add initial cv website files

---
+++ style.js
@@ -0,0 +1,13 @@
+// let main_pg = document.querySelector(".main_pg");
+// let contact_pg = document.querySelector(".contact_pg");
+// let send_btn = document.querySelector(".send_btn");
+// let contact_btn = document.querySelector(".contact_btn");
+
+// contact_btn.addEventListener("click", function () {
+//   main_pg.style.display = "none";
+//   contact_pg.style.display = "block";
+// });
+// send_btn.addEventListener("click", function () {
+//   contact_pg.style.display = "none";
+//   main_pg.style.display = "block";
+// });
